a1253df7b9b8f73a8025b1086124d5158586129c
Chris Kerbo <chris.kerbo@outlook.com>
2020-02-03 17:03:53 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ActiveRecord::Base
+  has_many :post_categories
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :categories
+  accepts_nested_attributes_for :categories, reject_if :all_blank
 
   def comments_attributes=(comment_attributes)
     comment_attributes.values.each do |comment_attribute|
